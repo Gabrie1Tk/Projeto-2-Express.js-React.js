11ea5f217a0a49d5cf72abfaf90dd35928ad3a13
Luiz Gustavo Garbelini Ramos <luizgustavogarbeliniramos@gmail.com>
2026-06-24 21:54:46 -0300

    Update vite.config.js to include Brotli compression for better performance.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Projeto-1---React.js/',
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024, // comprime arquivos maiores que 1KB
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1024,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+  },
 })
